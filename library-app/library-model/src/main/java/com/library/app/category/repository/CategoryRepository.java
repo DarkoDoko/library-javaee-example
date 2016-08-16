6216744d7f1c325d726013979aae031a43960da6
Darko Doko <darko.doko1@gmail.com>
2016-08-16 23:51:11 +0200

    Added Category Repository
    Added class responsible for data layer communication regarding the
    Category entity.

---
+++ library-app/library-model/src/main/java/com/library/app/category/repository/CategoryRepository.java
@@ -0,0 +1,22 @@
+package com.library.app.category.repository;
+
+import com.library.app.category.model.Category;
+import javax.persistence.EntityManager;
+
+/**
+ *
+ * @author ddoko
+ */
+public class CategoryRepository {
+    
+    EntityManager em;
+    
+    public Category add(Category category){
+        em.persist(category);
+        return category;
+    }
+    
+    public Category findById(Long id){
+        return em.find(Category.class, id);
+    }
+}
