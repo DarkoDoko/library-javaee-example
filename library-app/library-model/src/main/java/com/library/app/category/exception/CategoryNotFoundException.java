8fbdf693260f9e16de021abe619d16ee8df97e8b
Darko Doko <darko.doko1@gmail.com>
2016-08-23 23:22:51 +0200

    Added Category Not Found exception

---
+++ library-app/library-model/src/main/java/com/library/app/category/exception/CategoryNotFoundException.java
@@ -0,0 +1,10 @@
+package com.library.app.category.exception;
+
+/**
+ *
+ * @author ddoko
+ */
+public class CategoryNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 6068378752576978396L;
+    
+}
