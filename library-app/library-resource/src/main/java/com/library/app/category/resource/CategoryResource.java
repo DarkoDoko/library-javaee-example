483973d1e077b6a0d7e3435db27a6cb6b5d3d758
Darko Doko <darko.doko1@gmail.com>
2016-08-24 01:45:30 +0200

    Added class CategoryResource

---
+++ library-app/library-resource/src/main/java/com/library/app/category/resource/CategoryResource.java
@@ -0,0 +1,11 @@
+package com.library.app.category.resource;
+
+import com.library.app.category.services.CategoryServices;
+
+/**
+ *
+ * @author ddoko
+ */
+public class CategoryResource {
+    CategoryServices services;
+}
