b1dbf5cff277021ff9baae1a9217ac6aacc52e76
Darko Doko <darko.doko1@gmail.com>
2016-08-31 23:47:44 +0200

    Common test file name utils

---
+++ library-app/library-resource/src/test/java/com/library/app/commontests/utils/FileTestNameUtils.java
@@ -0,0 +1,23 @@
+package com.library.app.commontests.utils;
+
+import org.junit.Ignore;
+
+/**
+ *
+ * @author ddoko
+ */
+@Ignore
+public class FileTestNameUtils {
+    private static final String PATH_REQUEST = "/request/";
+    private static final String PATH_RESPONSE = "/response/";
+    
+    private FileTestNameUtils(){}
+    
+    public static String getPathFileRequest(String mainFolder, String fileName){
+        return mainFolder + PATH_REQUEST + fileName;
+    }
+    
+    public static String getPathFileResponse(String mainFolder, String fileName){
+        return mainFolder + PATH_RESPONSE + fileName;
+    }
+}
