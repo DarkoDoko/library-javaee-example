74d3eaf3b80ebdb5c5747f46be8e38191ac0ef8d
Darko Doko <darko.doko1@gmail.com>
2016-09-06 23:31:53 +0200

    typo fix

--- library-app/library-integration-tests/src/test/java/com/library/app/category/resource/CategoryResourceIntTest.java
+++ library-app/library-integration-tests/src/test/java/com/library/app/category/resource/CategoryResourceIntTest.java
@@ -40,9 +40,9 @@ public class CategoryResourceIntTest {
     public static WebArchive createDeployment(){
         return ShrinkWrap.create(WebArchive.class)
                 .addPackages(true, "com.library.app")
-                .addAsResource("persistance-integration.xml", "META-INF/persistance.xml")
+                .addAsResource("persistence-integration.xml", "META-INF/persistence.xml")
                 .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-                .setWebXML(new File("src/test/resources.web.xml"))
+                .setWebXML(new File("src/test/resources/web.xml"))
                 .addAsLibraries(Maven.resolver().resolve("com.google.code.gson:gson:2.3.1", "org.mockito:mockito-core:1.9.5").withTransitivity().asFile());
     }
     
