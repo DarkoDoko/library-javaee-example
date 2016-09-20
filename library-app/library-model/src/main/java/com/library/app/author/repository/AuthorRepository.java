b6aa9fd1f3632018508bb62b28d95aef2a18fef0
Darko Doko <darko.doko1@gmail.com>
2016-09-20 23:03:02 +0200

    added PersistenceContext annotation

--- library-app/library-model/src/main/java/com/library/app/author/repository/AuthorRepository.java
+++ library-app/library-model/src/main/java/com/library/app/author/repository/AuthorRepository.java
@@ -3,10 +3,12 @@ package com.library.app.author.repository;
 import com.library.app.author.model.Author;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Stateless
 public class AuthorRepository {
 
+    @PersistenceContext
     EntityManager em;
 
     Author add(Author author) {
