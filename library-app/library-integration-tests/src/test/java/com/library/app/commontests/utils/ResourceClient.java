0f66e6f4424414452dc429dba8f2de4b453aaf76
Darko Doko <darko.doko1@gmail.com>
2016-10-16 19:42:33 +0200

    formatting

--- library-app/library-integration-tests/src/test/java/com/library/app/commontests/utils/ResourceClient.java
+++ library-app/library-integration-tests/src/test/java/com/library/app/commontests/utils/ResourceClient.java
@@ -13,59 +13,59 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 public class ResourceClient {
-	private URL urlBase;
-	private String resourcePath;
+    private URL urlBase;
+    private String resourcePath;
 
-	public ResourceClient(final URL urlBase) {
-		this.urlBase = urlBase;
-	}
+    public ResourceClient(final URL urlBase) {
+            this.urlBase = urlBase;
+    }
 
-	public ResourceClient resourcePath(final String resourcePath) {
-		this.resourcePath = resourcePath;
-		return this;
-	}
+    public ResourceClient resourcePath(final String resourcePath) {
+            this.resourcePath = resourcePath;
+            return this;
+    }
 
-	public Response postWithFile(final String fileName) {
-		return postWithContent(getRequestFromFileOrEmptyIfNullFile(fileName));
-	}
+    public Response postWithFile(final String fileName) {
+            return postWithContent(getRequestFromFileOrEmptyIfNullFile(fileName));
+    }
 
-	public Response postWithContent(final String content) {
-		return buildClient().post(Entity.entity(content, MediaType.APPLICATION_JSON));
-	}
-        
-        public Response putWithFile(final String fileName) {
-		return putWithContent(getRequestFromFileOrEmptyIfNullFile(fileName));
-	}
+    public Response postWithContent(final String content) {
+            return buildClient().post(Entity.entity(content, MediaType.APPLICATION_JSON));
+    }
 
-	public Response putWithContent(final String content) {
-		return buildClient().put(Entity.entity(content, MediaType.APPLICATION_JSON));
-	}
-        
-        public void delete() {
-            buildClient().delete();
-	}
+    public Response putWithFile(final String fileName) {
+            return putWithContent(getRequestFromFileOrEmptyIfNullFile(fileName));
+    }
 
-	public Response get() {
-		return buildClient().get();
-	}
+    public Response putWithContent(final String content) {
+            return buildClient().put(Entity.entity(content, MediaType.APPLICATION_JSON));
+    }
 
-	private Builder buildClient() {
-		final Client resourceClient = ClientBuilder.newClient();
-		return resourceClient.target(getFullURL(resourcePath)).request();
-	}
+    public void delete() {
+        buildClient().delete();
+    }
 
-	private String getFullURL(final String resourcePath) {
-		try {
-                    return this.urlBase.toURI() + "api/" + resourcePath;
-		} catch (final URISyntaxException e) {
-                    throw new IllegalArgumentException(e);
-		}
-	}
+    public Response get() {
+            return buildClient().get();
+    }
 
-	private String getRequestFromFileOrEmptyIfNullFile(final String fileName) {
-		if (fileName == null) {
-			return "";
-		}
-		return readJsonFile(fileName);
-	}
+    private Builder buildClient() {
+            final Client resourceClient = ClientBuilder.newClient();
+            return resourceClient.target(getFullURL(resourcePath)).request();
+    }
+
+    private String getFullURL(final String resourcePath) {
+            try {
+                return this.urlBase.toURI() + "api/" + resourcePath;
+            } catch (final URISyntaxException e) {
+                throw new IllegalArgumentException(e);
+            }
+    }
+
+    private String getRequestFromFileOrEmptyIfNullFile(final String fileName) {
+            if (fileName == null) {
+                    return "";
+            }
+            return readJsonFile(fileName);
+    }
 }
