92822410d79179b929880c99ddb90628dfe378bb
Darko Doko <darko.doko1@gmail.com>
2016-10-16 18:13:01 +0200

    Removed unused imports

--- library-app/library-resource/src/main/java/com/library/app/category/resource/CategoryJsonConverter.java
+++ library-app/library-resource/src/main/java/com/library/app/category/resource/CategoryJsonConverter.java
@@ -1,12 +1,10 @@
 package com.library.app.category.resource;
 
-import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.library.app.json.JsonReader;
 import com.library.app.category.model.Category;
 import com.library.app.json.EntityJsonConverter;
-import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
