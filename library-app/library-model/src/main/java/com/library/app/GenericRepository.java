06f3a7fc2646876e3e6cd9078ced033faa367bc0
Darko Doko <darko.doko1@gmail.com>
2016-10-16 19:08:25 +0200

    formatting

--- library-app/library-model/src/main/java/com/library/app/GenericRepository.java
+++ library-app/library-model/src/main/java/com/library/app/GenericRepository.java
@@ -6,55 +6,59 @@ import javax.persistence.Query;
 
 public abstract class GenericRepository<T> {
 
-	protected abstract Class<T> getPersistentClass();
+    protected abstract Class<T> getPersistentClass();
 
-	protected abstract EntityManager getEntityManager();
+    protected abstract EntityManager getEntityManager();
 
-	public T add(T entity) {
-		getEntityManager().persist(entity);
-		return entity;
-	}
+    public T add(T entity) {
+        getEntityManager().persist(entity);
+        return entity;
+    }
 
-	public T findById(Long id) {
-		if (id == null) {
-			return null;
-		}
-		return getEntityManager().find(getPersistentClass(), id);
-	}
+    public T findById(Long id) {
+        if (id == null) {
+            return null;
+        }
+        return getEntityManager().find(getPersistentClass(), id);
+    }
 
-	public void update(T entity) {
-		getEntityManager().merge(entity);
-	}
+    public void update(T entity) {
+        getEntityManager().merge(entity);
+    }
 
-	public List<T> findAll(String orderField) {
-		return getEntityManager().createQuery(
-				"Select e From " + getPersistentClass().getSimpleName() + " e Order by e." + orderField)
-				.getResultList();
-	}
+    public List<T> findAll(String orderField) {
+        return getEntityManager().createQuery(
+                        "Select e From " + getPersistentClass().getSimpleName() + " e Order by e." + orderField)
+                        .getResultList();
+    }
 
-	public boolean alreadyExists(String propertyName, String propertyValue, Long id) {
-		StringBuilder jpql = new StringBuilder();
-		jpql.append("Select 1 From " + getPersistentClass().getSimpleName() + " e where e." + propertyName
-				+ " = :propertyValue");
-		if (id != null) {
-			jpql.append(" and e.id != :id");
-		}
+    public boolean alreadyExists(String propertyName, String propertyValue, Long id) {
+        StringBuilder jpql = new StringBuilder();
+        jpql.append("Select 1 From ")
+            .append(getPersistentClass().getSimpleName())
+            .append(" e where e.")
+            .append(propertyName)
+            .append(" = :propertyValue");
+        
+        if (id != null) {
+            jpql.append(" and e.id != :id");
+        }
 
-		Query query = getEntityManager().createQuery(jpql.toString());
-		query.setParameter("propertyValue", propertyValue);
-		if (id != null) {
-			query.setParameter("id", id);
-		}
+        Query query = getEntityManager().createQuery(jpql.toString());
+        query.setParameter("propertyValue", propertyValue);
+        if (id != null) {
+            query.setParameter("id", id);
+        }
 
-		return query.setMaxResults(1).getResultList().size() > 0;
-	}
+        return query.setMaxResults(1).getResultList().size() > 0;
+    }
 
-	public boolean existsById(Long id) {
-		return getEntityManager()
-				.createQuery("Select 1 From " + getPersistentClass().getSimpleName() + " e where e.id = :id")
-				.setParameter("id", id)
-				.setMaxResults(1)
-				.getResultList().size() > 0;
-	}
+    public boolean existsById(Long id) {
+        return getEntityManager()
+                        .createQuery("Select 1 From " + getPersistentClass().getSimpleName() + " e where e.id = :id")
+                        .setParameter("id", id)
+                        .setMaxResults(1)
+                        .getResultList().size() > 0;
+    }
     
 }
