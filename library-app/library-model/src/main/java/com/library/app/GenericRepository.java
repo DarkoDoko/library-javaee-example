a15c9baa52e4a671bbf10f9b945117800eeb6a78
Darko Doko <darko.doko1@gmail.com>
2016-10-23 01:18:55 +0200

    Refactoring: Added pagination filter search in generic repository

--- library-app/library-model/src/main/java/com/library/app/GenericRepository.java
+++ library-app/library-model/src/main/java/com/library/app/GenericRepository.java
@@ -1,8 +1,11 @@
 package com.library.app;
 
 import java.util.List;
+import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import com.library.app.pagination.PaginatedData;
+import com.library.app.pagination.filter.PaginationData;
 
 public abstract class GenericRepository<T> {
 
@@ -28,20 +31,20 @@ public abstract class GenericRepository<T> {
 
     public List<T> findAll(String orderField) {
         return getEntityManager().createQuery(
-                        "Select e From " + getPersistentClass().getSimpleName() + " e Order by e." + orderField)
-                        .getResultList();
+                "SELECT e FROM " + getPersistentClass().getSimpleName() + " e ORDER BY e." + orderField)
+                .getResultList();
     }
 
     public boolean alreadyExists(String propertyName, String propertyValue, Long id) {
         StringBuilder jpql = new StringBuilder();
-        jpql.append("Select 1 From ")
-            .append(getPersistentClass().getSimpleName())
-            .append(" e where e.")
-            .append(propertyName)
-            .append(" = :propertyValue");
-        
+        jpql.append("SELECT 1 FROM ")
+                .append(getPersistentClass().getSimpleName())
+                .append(" e WHERE e.")
+                .append(propertyName)
+                .append(" = :propertyValue");
+
         if (id != null) {
-            jpql.append(" and e.id != :id");
+            jpql.append(" AND e.id != :id");
         }
 
         Query query = getEntityManager().createQuery(jpql.toString());
@@ -55,10 +58,55 @@ public abstract class GenericRepository<T> {
 
     public boolean existsById(Long id) {
         return getEntityManager()
-                        .createQuery("Select 1 From " + getPersistentClass().getSimpleName() + " e where e.id = :id")
-                        .setParameter("id", id)
-                        .setMaxResults(1)
-                        .getResultList().size() > 0;
+                .createQuery("SELECT 1 FROM " + getPersistentClass().getSimpleName() + " e WHERE e.id = :id")
+                .setParameter("id", id)
+                .setMaxResults(1)
+                .getResultList().size() > 0;
     }
-    
+
+    protected PaginatedData<T> findByParameters(String clause, PaginationData paginationData,
+            Map<String, Object> queryParameters, String defaultSortFieldWithDirection) {
+        String clauseSort = "ORDER BY e." + getSortField(paginationData, defaultSortFieldWithDirection);
+        Query queryEntities = getEntityManager().createQuery(
+                "SELECT e FROM " + getPersistentClass().getSimpleName()
+                + " e " + clause + " " + clauseSort);
+        applyQueryParametersOnQuery(queryParameters, queryEntities);
+        applyPaginationOnQuery(paginationData, queryEntities);
+
+        List<T> entities = queryEntities.getResultList();
+
+        return new PaginatedData<>(countWithFilter(clause, queryParameters), entities);
+    }
+
+    private int countWithFilter(String clause, Map<String, Object> queryParameters) {
+        Query queryCount = getEntityManager().createQuery(
+                "SELECT COUNT(e) FROM " + getPersistentClass().getSimpleName() + " e " + clause);
+        applyQueryParametersOnQuery(queryParameters, queryCount);
+        return ((Long) queryCount.getSingleResult()).intValue();
+    }
+
+    private void applyPaginationOnQuery(PaginationData paginationData, Query query) {
+        if (paginationData != null) {
+            query.setFirstResult(paginationData.getFirstResult());
+            query.setMaxResults(paginationData.getMaxResults());
+        }
+    }
+
+    private String getSortField(PaginationData paginationData, String defaultSortField) {
+        if (paginationData == null || paginationData.getOrderField() == null) {
+            return defaultSortField;
+        }
+        return paginationData.getOrderField() + " " + getSortDirection(paginationData);
+    }
+
+    private String getSortDirection(PaginationData paginationData) {
+        return paginationData.isAscending() ? "ASC" : "DESC";
+    }
+
+    private void applyQueryParametersOnQuery(Map<String, Object> queryParameters, Query query) {
+        queryParameters.entrySet().forEach((entryMap) -> {
+            query.setParameter(entryMap.getKey(), entryMap.getValue());
+        });
+    }
+
 }
--- library-app/library-model/src/main/java/com/library/app/author/repository/AuthorRepository.java
+++ library-app/library-model/src/main/java/com/library/app/author/repository/AuthorRepository.java
@@ -5,13 +5,10 @@ import com.library.app.author.model.Author;
 import com.library.app.author.model.AuthorFilter;
 import com.library.app.pagination.PaginatedData;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
 @Stateless
 public class AuthorRepository extends GenericRepository<Author>{
@@ -39,36 +36,6 @@ public class AuthorRepository extends GenericRepository<Author>{
             queryParameters.put("name", "%" + filter.getName() + "%");
         }
         
-        StringBuilder clauseSort = new StringBuilder();
-        if(filter.hasOrderField()){
-            clauseSort.append("ORDER BY e." + filter.getPaginationData().getOrderField());
-            clauseSort.append(filter.getPaginationData().isAscending() ? " ASC" : " DESC");
-        } else {
-            clauseSort.append("ORDER BY e.name ASC");
-        }
-        
-        Query queryAuthors = em.createQuery("SELECT e FROM Author e " + clause.toString() + " " + clauseSort.toString());
-        applyParametersOnQuery(queryParameters, queryAuthors);
-        
-        if(filter.hasPaginationData()){
-            queryAuthors.setFirstResult(filter.getPaginationData().getFirstResult());
-            queryAuthors.setMaxResults(filter.getPaginationData().getMaxResults());
-        }
-        
-        List<Author> authors = queryAuthors.getResultList();
-        
-        Query queryCount = em.createQuery("SELECT Count(e) FROM Author e " + clause.toString());
-        applyParametersOnQuery(queryParameters, queryCount);
-        
-        Integer count = ((Long) queryCount.getSingleResult()).intValue();
-        
-        return new PaginatedData<>(count, authors);
+        return findByParameters(clause.toString(), filter.getPaginationData(), queryParameters, "name ASC");
     }
-
-    private void applyParametersOnQuery(Map<String, Object> queryParameters, Query query) {
-        queryParameters.entrySet().forEach((entryMap) -> {
-            query.setParameter(entryMap.getKey(), entryMap.getValue());
-        });
-    }
-    
 }
