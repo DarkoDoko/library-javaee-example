102e28e6f4ac55a647a77a00c757043856c1b67c
Darko Doko <darko.doko1@gmail.com>
2016-10-29 15:40:54 +0200

    Check if user with given email already exists.

--- library-app/library-resource/src/main/java/com/library/app/user/resource/UserResource.java
+++ library-app/library-resource/src/main/java/com/library/app/user/resource/UserResource.java
@@ -3,8 +3,11 @@ package com.library.app.user.resource;
 import com.library.app.common.model.HttpCode;
 import com.library.app.common.model.OperationResult;
 import com.library.app.common.model.ResourceMessage;
+import com.library.app.common.model.StandardsOperationResults;
+import static com.library.app.common.model.StandardsOperationResults.getOperationResultExistent;
 import com.library.app.json.JsonUtils;
 import com.library.app.json.OperationResultJsonWriter;
+import com.library.app.user.UserExistentException;
 import com.library.app.user.model.User;
 import com.library.app.user.model.User.UserType;
 import com.library.app.user.services.UserServices;
@@ -27,39 +30,48 @@ import org.slf4j.LoggerFactory;
 public class UserResource {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
-    
+
     private static final ResourceMessage RESOURCE_MESSAGE = new ResourceMessage("user");
 
     @Inject
     UserJsonConverter jsonConverter;
-    
+
     @Inject
     UserServices services;
-    
+
     @Context
     SecurityContext securityContext;
-    
+
     @Context
     UriInfo uriInfo;
-    
+
     @POST
     public Response add(String body) {
         logger.debug("Adding a new user with body {}", body);
-        
+
         User user = jsonConverter.convertFrom(body);
-        
-        if(user.getUserType().equals(UserType.EMPLOYEE)) {
+
+        if (user.getUserType().equals(UserType.EMPLOYEE)) {
             return Response.status(HttpCode.FORBIDDEN.getCode()).build();
         }
-        
-        user = services.add(user);
-        OperationResult result = OperationResult.success(JsonUtils.getJsonElementWithId(user.getId()));
-        
+
+        HttpCode httpCode = HttpCode.CREATED;
+        OperationResult result;
+        try {
+            user = services.add(user);
+            result = OperationResult.success(JsonUtils.getJsonElementWithId(user.getId()));
+        } catch( UserExistentException e) {
+            
+            httpCode = HttpCode.VALIDATION_ERROR;
+            logger.error("There is already an user for the given email", e);
+            result = getOperationResultExistent(RESOURCE_MESSAGE, "email");
+        }
+
         logger.debug("Returning the operation result after adding user: {}", result);
         return Response.
-                    status(HttpCode.CREATED.getCode()).
-                    entity(OperationResultJsonWriter.toJson(result)).
-                    build();
+            status(httpCode.getCode()).
+            entity(OperationResultJsonWriter.toJson(result)).
+            build();
     }
-    
+
 }
--- library-app/library-resource/src/test/java/com/library/app/user/resource/UserResourceTest.java
+++ library-app/library-resource/src/test/java/com/library/app/user/resource/UserResourceTest.java
@@ -2,10 +2,13 @@ package com.library.app.user.resource;
 
 import com.library.app.common.model.HttpCode;
 import static com.library.app.commontests.utils.FileTestNameUtils.getPathFileRequest;
+import static com.library.app.commontests.utils.FileTestNameUtils.getPathFileResponse;
 import com.library.app.commontests.utils.JsonTestUtils;
 import static com.library.app.commontests.utils.JsonTestUtils.assertJsonMatchesExpectedJson;
+import static com.library.app.commontests.utils.JsonTestUtils.assertJsonMatchesFileContent;
 import static com.library.app.commontests.utils.JsonTestUtils.readJsonFile;
 import static com.library.app.user.UserArgumentMatcher.userEq;
+import com.library.app.user.UserExistentException;
 import static com.library.app.user.UserForTestsRepository.johnDoe;
 import static com.library.app.user.UserForTestsRepository.userWithIdAndCreatedAt;
 import com.library.app.user.services.UserServices;
@@ -53,7 +56,7 @@ public class UserResourceTest {
             thenReturn(userWithIdAndCreatedAt(johnDoe(), 1L));
 
         Response response = resourceUnderTest.add(readJsonFile(
-                getPathFileRequest(PATH_RESOURCE, "customerJohnDoe.json")));
+            getPathFileRequest(PATH_RESOURCE, "customerJohnDoe.json")));
 
         assertThat(response.getStatus(), is(equalTo(HttpCode.CREATED.getCode())));
         assertJsonMatchesExpectedJson(response.getEntity().toString(), "{\"id\": 1}");
@@ -61,16 +64,31 @@ public class UserResourceTest {
 
     @Test
     public void addValidEmployee() {
-        final Response response = resourceUnderTest.add(
-                readJsonFile(getPathFileRequest(PATH_RESOURCE, "employeeAdmin.json")));
+        Response response = resourceUnderTest.add(readJsonFile(
+            getPathFileRequest(PATH_RESOURCE, "employeeAdmin.json")));
+        
         assertThat(response.getStatus(), is(equalTo(HttpCode.FORBIDDEN.getCode())));
     }
 
-    private static String getJsonWithPassword(final String password) {
+    @Test
+    public void addExistentUser() {
+        when(servicesCollaborator.add(userEq(johnDoe()))).thenThrow(new UserExistentException());
+
+        Response response = resourceUnderTest.add(readJsonFile(getPathFileRequest(PATH_RESOURCE,
+            "customerJohnDoe.json")));
+        assertThat(response.getStatus(), is(equalTo(HttpCode.VALIDATION_ERROR.getCode())));
+        assertJsonResponseWithFile(response, "userAlreadyExists.json");
+    }
+
+    private static String getJsonWithPassword(String password) {
         return String.format("{\"password\":\"%s\"}", password);
     }
 
-    private static String getJsonWithEmailAndPassword(final String email, final String password) {
+    private static String getJsonWithEmailAndPassword(String email, String password) {
         return String.format("{\"email\":\"%s\",\"password\":\"%s\"}", email, password);
     }
+    
+    private void assertJsonResponseWithFile(Response response, String fileName) {
+		assertJsonMatchesFileContent(response.getEntity().toString(), getPathFileResponse(PATH_RESOURCE, fileName));
+	}
 }
