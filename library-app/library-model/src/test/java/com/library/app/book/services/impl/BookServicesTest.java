3bb8c34b9c57b06d5a05176ef6d4f14e727dc436
Darko Doko <darko.doko1@gmail.com>
2016-12-04 15:09:45 +0100

    Renamed Book Services test class

--- library-app/library-model/src/test/java/com/library/app/book/services/impl/BookServicesTest.java
+++ library-app/library-model/src/test/java/com/library/app/book/services/impl/BookServicesTest.java
@@ -1,9 +1,14 @@
 package com.library.app.book.services.impl;
 
 import com.library.app.FieldNotValidException;
+import static com.library.app.author.AuthorForTestsRepository.erichGamma;
+import static com.library.app.author.AuthorForTestsRepository.robertMartin;
 import com.library.app.author.AuthorNotFoundException;
 import com.library.app.author.services.AuthorServices;
+import static com.library.app.book.BookArgumentMatcher.bookEq;
+import static com.library.app.book.BookForTestsRepository.bookWithId;
 import static com.library.app.book.BookForTestsRepository.cleanCode;
+import static com.library.app.book.BookForTestsRepository.designPatterns;
 import com.library.app.book.model.Book;
 import com.library.app.book.repository.BookRepository;
 import com.library.app.book.services.BookServices;
@@ -21,10 +26,12 @@ import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.mockito.Matchers.anyLong;
 import org.mockito.Mock;
+import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 
-public class BookServicesImplTest {
+public class BookServicesTest {
     
     private static Validator validator;
     private BookServices bookServicesUnderTest;
@@ -90,6 +97,39 @@ public class BookServicesImplTest {
 		addBookWithInvalidField(book, "price");
 	}
     
+    @Test(expected = CategoryNotFoundException.class)
+	public void addBookWithInexistentCategory() throws Exception{
+		when(categoryServices.findById(1L)).thenThrow(new CategoryNotFoundException());
+
+		final Book book = cleanCode();
+		book.getCategory().setId(1L);
+
+        bookServicesUnderTest.add(book);
+	}
+
+	@Test(expected = AuthorNotFoundException.class)
+	public void addBookWithInexistentAuthor() throws Exception {
+		when(categoryServices.findById(anyLong())).thenReturn(designPatterns().getCategory());
+		when(authorServices.findById(1L)).thenReturn(erichGamma());
+		when(authorServices.findById(2L)).thenThrow(new AuthorNotFoundException());
+
+		final Book book = designPatterns();
+		book.getAuthors().get(0).setId(1L);
+		book.getAuthors().get(1).setId(2L);
+
+		bookServicesUnderTest.add(book);
+	}
+
+	@Test
+	public void addValidBook() throws Exception {
+		when(categoryServices.findById(anyLong())).thenReturn(cleanCode().getCategory());
+		when(authorServices.findById(anyLong())).thenReturn(robertMartin());
+		when(bookRepository.add(bookEq(cleanCode()))).thenReturn(bookWithId(cleanCode(), 1L));
+
+		final Book bookAdded = bookServicesUnderTest.add(cleanCode());
+		assertThat(bookAdded.getId(), equalTo(1L));
+	}
+    
     private void addBookWithInvalidField(final Book book, final String expectedInvalidFieldName) {
 		try {
 			bookServicesUnderTest.add(book);
@@ -97,9 +137,9 @@ public class BookServicesImplTest {
 		} catch (final FieldNotValidException e) {
 			assertThat(e.getFieldName(), is(equalTo(expectedInvalidFieldName)));
 		} catch (CategoryNotFoundException ex) {
-            Logger.getLogger(BookServicesImplTest.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(BookServicesTest.class.getName()).log(Level.SEVERE, null, ex);
         } catch (AuthorNotFoundException ex) {
-            Logger.getLogger(BookServicesImplTest.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(BookServicesTest.class.getName()).log(Level.SEVERE, null, ex);
         }
 	}
     
