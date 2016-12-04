597a98f96b3013e2618af1895bd47dc610955ab3
Darko Doko <darko.doko1@gmail.com>
2016-12-04 19:19:05 +0100

    Added update operation in Book Resources

--- library-app/library-resource/src/main/java/com/library/app/book/resource/BookResource.java
+++ library-app/library-resource/src/main/java/com/library/app/book/resource/BookResource.java
@@ -2,6 +2,7 @@ package com.library.app.book.resource;
 
 import com.library.app.FieldNotValidException;
 import com.library.app.author.AuthorNotFoundException;
+import com.library.app.book.BookNotFoundException;
 import com.library.app.book.model.Book;
 import com.library.app.book.services.BookServices;
 import com.library.app.category.CategoryNotFoundException;
@@ -10,13 +11,16 @@ import com.library.app.common.model.OperationResult;
 import com.library.app.common.model.ResourceMessage;
 import static com.library.app.common.model.StandardsOperationResults.getOperationResultDependencyNotFound;
 import static com.library.app.common.model.StandardsOperationResults.getOperationResultInvalidField;
+import static com.library.app.common.model.StandardsOperationResults.getOperationResultNotFound;
 import com.library.app.json.JsonUtils;
 import com.library.app.json.OperationResultJsonWriter;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -68,6 +72,40 @@ public class BookResource {
 		}
 
 		logger.debug("Returning the operation result after adding book: {}", result);
+		return Response.status(httpCode.getCode()).entity(OperationResultJsonWriter.toJson(result)).build();
+	}
+    
+    @PUT
+	@Path("/{id}")
+	public Response update(@PathParam("id") final Long id, final String body) {
+		logger.debug("Updating the book {} with body {}", id, body);
+		final Book book = bookJsonConverter.convertFrom(body);
+		book.setId(id);
+
+		HttpCode httpCode = HttpCode.OK;
+		OperationResult result;
+		try {
+			bookServices.update(book);
+			result = OperationResult.success();
+		} catch (final FieldNotValidException e) {
+			httpCode = HttpCode.VALIDATION_ERROR;
+			logger.error("One of the fields of the book is not valid", e);
+			result = getOperationResultInvalidField(RESOURCE_MESSAGE, e);
+		} catch (final CategoryNotFoundException e) {
+			httpCode = HttpCode.VALIDATION_ERROR;
+			logger.error("Category not found for book", e);
+			result = getOperationResultDependencyNotFound(RESOURCE_MESSAGE, "category");
+		} catch (final AuthorNotFoundException e) {
+			httpCode = HttpCode.VALIDATION_ERROR;
+			logger.error("Author not found for book", e);
+			result = getOperationResultDependencyNotFound(RESOURCE_MESSAGE, "author");
+		} catch (final BookNotFoundException e) {
+			httpCode = HttpCode.NOT_FOUND;
+			logger.error("No book found for the given id", e);
+			result = getOperationResultNotFound(RESOURCE_MESSAGE);
+		}
+
+		logger.debug("Returning the operation result after updating book: {}", result);
 		return Response.status(httpCode.getCode()).entity(OperationResultJsonWriter.toJson(result)).build();
 	}
     
--- library-app/library-resource/src/test/java/com/library/app/book/resource/BookResourceTest.java
+++ library-app/library-resource/src/test/java/com/library/app/book/resource/BookResourceTest.java
@@ -7,6 +7,7 @@ import com.library.app.author.resource.AuthorJsonConverter;
 import static com.library.app.book.BookArgumentMatcher.bookEq;
 import static com.library.app.book.BookForTestsRepository.bookWithId;
 import static com.library.app.book.BookForTestsRepository.cleanCode;
+import com.library.app.book.BookNotFoundException;
 import com.library.app.book.model.Book;
 import com.library.app.book.services.BookServices;
 import com.library.app.category.CategoryNotFoundException;
@@ -28,6 +29,7 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Matchers.anyObject;
 import org.mockito.Mock;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 
@@ -49,7 +51,7 @@ public class BookResourceTest {
 
 		bookResourceUnderTest = new BookResource();
 
-		final BookJsonConverter bookJsonConverter = new BookJsonConverter();
+		BookJsonConverter bookJsonConverter = new BookJsonConverter();
 		bookJsonConverter.categoryJsonConverter = new CategoryJsonConverter();
 		bookJsonConverter.authorJsonConverter = new AuthorJsonConverter();
 
@@ -61,12 +63,12 @@ public class BookResourceTest {
     
     @Test
 	public void addValidBook() throws Exception {
-		final Book expectedBook = cleanCode();
+		Book expectedBook = cleanCode();
 		expectedBook.setCategory(new Category(1L));
 		expectedBook.setAuthors(Arrays.asList(new Author(2L)));
 		when(bookServices.add(bookEq(expectedBook))).thenReturn(bookWithId(cleanCode(), 1L));
 
-		final Response response = bookResourceUnderTest.add(readJsonFile(getPathFileRequest(PATH_RESOURCE, "cleanCode.json")));
+		Response response = bookResourceUnderTest.add(readJsonFile(getPathFileRequest(PATH_RESOURCE, "cleanCode.json")));
 		assertThat(response.getStatus(), is(equalTo(HttpCode.CREATED.getCode())));
 		assertJsonMatchesExpectedJson(response.getEntity().toString(), "{\"id\": 1}");
 	}
@@ -88,6 +90,30 @@ public class BookResourceTest {
 		addBookWithValidationError(new AuthorNotFoundException(), "cleanCode.json", "bookErrorInexistentAuthor.json");
 	}
     
+    @Test
+	public void updateBookWithNullTitle() throws Exception {
+		updateBookWithError(new FieldNotValidException("title", "may not be null"), HttpCode.VALIDATION_ERROR,
+				"cleanCode.json", "bookErrorNullTitle.json");
+	}
+
+	@Test
+	public void updateBookWithInexistentCategory() throws Exception {
+		updateBookWithError(new CategoryNotFoundException(), HttpCode.VALIDATION_ERROR, "cleanCode.json",
+				"bookErrorInexistentCategory.json");
+	}
+
+	@Test
+	public void updateBookWithInexistentAuthor() throws Exception {
+		updateBookWithError(new AuthorNotFoundException(), HttpCode.VALIDATION_ERROR, "cleanCode.json",
+				"bookErrorInexistentAuthor.json");
+	}
+
+	@Test
+	public void updateBookNotFound() throws Exception {
+		updateBookWithError(new BookNotFoundException(), HttpCode.NOT_FOUND, "cleanCode.json",
+				"bookErrorInexistentAuthor.json");
+	}
+    
     private void addBookWithValidationError(final Exception exceptionToBeThrown, final String requestFileName,
 			final String responseFileName) throws Exception {
 		when(bookServices.add((Book) anyObject())).thenThrow(exceptionToBeThrown);
@@ -95,6 +121,18 @@ public class BookResourceTest {
 		Response response = bookResourceUnderTest.add(readJsonFile(getPathFileRequest(PATH_RESOURCE, requestFileName)));
 		assertThat(response.getStatus(), is(equalTo(HttpCode.VALIDATION_ERROR.getCode())));
 		assertJsonResponseWithFile(response, responseFileName);
+	}
+    
+    private void updateBookWithError(Exception exceptionToBeThrown, HttpCode expectedHttpCode,String requestFileName,
+			String responseFileName) throws Exception {
+		doThrow(exceptionToBeThrown).when(bookServices).update(bookWithId(cleanCode(), 1L));
+
+		Response response = bookResourceUnderTest.update(1L, 
+            readJsonFile(getPathFileRequest(PATH_RESOURCE, requestFileName)));
+		assertThat(response.getStatus(), is(equalTo(expectedHttpCode.getCode())));
+		if (expectedHttpCode != HttpCode.NOT_FOUND) {
+			assertJsonResponseWithFile(response, responseFileName);
+		}
 	}
     
     private void assertJsonResponseWithFile(final Response response, final String fileName) {
