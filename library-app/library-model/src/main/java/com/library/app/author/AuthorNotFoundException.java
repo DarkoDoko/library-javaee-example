1b4afe0fbeffc84477767e8f3d2f21fe7ee76a8b
Darko Doko <darko.doko1@gmail.com>
2016-12-04 13:34:32 +0100

    Author Not Found exception is a Runtime Exception

--- library-app/library-model/src/main/java/com/library/app/author/AuthorNotFoundException.java
+++ library-app/library-model/src/main/java/com/library/app/author/AuthorNotFoundException.java
@@ -1,5 +1,8 @@
 package com.library.app.author;
 
-public class AuthorNotFoundException extends Exception {
+import javax.ejb.ApplicationException;
+
+@ApplicationException
+public class AuthorNotFoundException extends RuntimeException {
     private static final long serialVersionUID = -8526377028109307421L;
 }
