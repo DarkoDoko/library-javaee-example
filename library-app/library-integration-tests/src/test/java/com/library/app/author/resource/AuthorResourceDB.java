9ccb30553cf36a257e7fc5c1f6ecb0b89a50b4c2
Darko Doko <darko.doko1@gmail.com>
2016-12-17 10:56:30 +0100

    Added find by name for Author, Category

--- library-app/library-integration-tests/src/test/java/com/library/app/author/resource/AuthorResourceDB.java
+++ library-app/library-integration-tests/src/test/java/com/library/app/author/resource/AuthorResourceDB.java
@@ -1,12 +1,20 @@
 package com.library.app.author.resource;
 
 import com.library.app.author.AuthorForTestsRepository;
+import com.library.app.author.model.Author;
+import com.library.app.author.model.AuthorFilter;
 import com.library.app.author.services.AuthorServices;
+import com.library.app.common.model.HttpCode;
+import com.library.app.json.JsonWriter;
+import com.library.app.pagination.PaginatedData;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/DB/authors")
 @Produces(MediaType.APPLICATION_JSON)
@@ -15,9 +23,26 @@ public class AuthorResourceDB {
     @Inject
     private AuthorServices services;
     
+    private  AuthorJsonConverter jsonConverter;
+    
     @POST
     public void addAll() {
         AuthorForTestsRepository.allAuthors().forEach(services::add);
     }
     
+    @GET
+    @Path("/{name}")
+    public Response findByName(@PathParam("name") String name){
+        AuthorFilter filter = new AuthorFilter();
+        filter.setName(name);
+        
+        PaginatedData<Author> authors = services.findByFilter(filter);
+        if(authors.getNumberOfRows() > 0) {
+            Author author = authors.getRow(0);
+            String authorAsJson = JsonWriter.writeToString(jsonConverter.convertToJsonElement(author));
+            return Response.status(HttpCode.OK.getCode()).entity(authorAsJson).build();
+        }
+        
+        return Response.status(HttpCode.NOT_FOUND.getCode()).build();
+    }
 }
--- library-app/library-integration-tests/src/test/java/com/library/app/category/resource/CategoryResourceDB.java
+++ library-app/library-integration-tests/src/test/java/com/library/app/category/resource/CategoryResourceDB.java
@@ -1,12 +1,20 @@
 package com.library.app.category.resource;
 
 import com.library.app.category.CategoryForTestsRepository;
+import com.library.app.category.model.Category;
 import com.library.app.category.services.CategoryServices;
+import com.library.app.common.model.HttpCode;
+import com.library.app.json.JsonWriter;
+import java.util.List;
+import java.util.Optional;
 import javax.inject.Inject;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/DB/categories")
 @Produces(MediaType.APPLICATION_JSON)
@@ -15,8 +23,29 @@ public class CategoryResourceDB {
     @Inject
     private CategoryServices services;
     
+    @Inject
+    private CategoryJsonConverter jsonConverter;
+    
     @POST
     public void addAll(){
         CategoryForTestsRepository.allCategories().forEach(services::add);
     }
+    
+    @GET
+	@Path("/{name}")
+	public Response findByName(@PathParam("name") String name) {
+		List<Category> categories = services.findAll();
+
+        Optional<Category> category = categories.stream().
+                                                filter(c -> c.getName().equals(name)).
+                                                findFirst();
+		
+        if (category.isPresent()) {
+			String categoryAsJson = JsonWriter.writeToString(jsonConverter.convertToJsonElement(category.get()));
+			return Response.status(HttpCode.OK.getCode()).entity(categoryAsJson).build();
+		} else {
+			return Response.status(HttpCode.NOT_FOUND.getCode()).build();
+		}
+	}
+
 }
