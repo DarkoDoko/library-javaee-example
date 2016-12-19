b45357c8b59302875c06fd30ed636f3508f99914
Darko Doko <darko.doko1@gmail.com>
2016-12-19 01:05:05 +0100

    Added Book entity to deletion entities

--- library-app/library-model/src/test/java/com/library/app/commontests/utils/TestRepositoryEJB.java
+++ library-app/library-model/src/test/java/com/library/app/commontests/utils/TestRepositoryEJB.java
@@ -1,6 +1,7 @@
 package com.library.app.commontests.utils;
 
 import com.library.app.author.model.Author;
+import com.library.app.book.model.Book;
 import com.library.app.category.model.Category;
 import com.library.app.user.model.User;
 import java.util.Arrays;
@@ -17,7 +18,7 @@ public class TestRepositoryEJB {
     @PersistenceContext
     private EntityManager em;
     
-    private static final List<Class<?>> ENTITIES_TO_REMOVE = Arrays.asList(User.class, Category.class, Author.class);
+    private static final List<Class<?>> ENTITIES_TO_REMOVE = Arrays.asList(Book.class, User.class, Category.class, Author.class);
     
     public void deleteAll(){
         ENTITIES_TO_REMOVE.forEach((entityClass) -> {
