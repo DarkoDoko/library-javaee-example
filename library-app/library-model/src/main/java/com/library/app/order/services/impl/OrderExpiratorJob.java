9b4f99ee5824090b28c2aaefd75a349554ac72be
Darko Doko <darko.doko1@gmail.com>
2016-12-30 00:23:20 +0100

    Use PropertyValue annotation

--- library-app/library-model/src/main/java/com/library/app/order/services/impl/OrderExpiratorJob.java
+++ library-app/library-model/src/main/java/com/library/app/order/services/impl/OrderExpiratorJob.java
@@ -1,6 +1,7 @@
 package com.library.app.order.services.impl;
 
 import com.library.app.appproperties.ApplicationProperties;
+import com.library.app.appproperties.PropertyValue;
 import com.library.app.order.services.OrderServices;
 import javax.ejb.Schedule;
 import javax.ejb.Singleton;
@@ -14,15 +15,18 @@ public class OrderExpiratorJob {
     @Inject
     OrderServices orderServices;
     
-    @Inject
-    private ApplicationProperties applicationProperties;
+    //@Inject
+    //private ApplicationProperties applicationProperties;
+    
+    @PropertyValue(name = "days-before-order-expiration")
+    private Integer daysBeforeOrderExpiration;
     
     private Logger logger = LoggerFactory.getLogger(getClass());
     
     @Schedule(hour = "*/1", minute = "0", second = "0", persistent = false)
     public void run(){
         logger.debug("Executing order expirator job");
-        orderServices.changeStatusOfExpiredOrders(applicationProperties.getDaysBeforeExpiration());
+        orderServices.changeStatusOfExpiredOrders(daysBeforeOrderExpiration);
     }
     
 }
