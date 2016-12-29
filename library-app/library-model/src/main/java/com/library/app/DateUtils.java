bbd91985e1ba9ffa620f415b7f2c90f81c8c5c19
Darko Doko <darko.doko1@gmail.com>
2016-12-29 23:23:19 +0100

    Added method for days addition

--- library-app/library-model/src/main/java/com/library/app/DateUtils.java
+++ library-app/library-model/src/main/java/com/library/app/DateUtils.java
@@ -2,10 +2,17 @@ package com.library.app;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 public final class DateUtils {
     private static final String FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+
+    public static Date currentDatePlusDays(int days) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        return Date.from(localDateTime.plusDays(days).atZone(ZoneId.systemDefault()).toInstant());
+    }
     
     private DateUtils() {    
     }
